79979991cf40c79d1f4c2bd92e45fbe39eaa53bd
macbook <muhammadwaqas0978@gmail.com>
2023-05-30 22:26:34 +0500

    delete api added

--- routes/upload.js
+++ routes/upload.js
@@ -45,6 +45,29 @@ router.post('/uploadBackgrounds', upload.single('file'), function (req, res) {
   });
 });
 
+router.delete('/deleteBackground/:id', function (req, res) {
+  const id = req.params.id;
+
+  if (!id) {
+    res.status(200).json({ status: 400, message: 'Id must be required' });
+    return;
+  }
+
+  const params = {
+    Bucket: 'posters-assets',
+    Key: "backgrounds/" + id + ".png",
+  };
+
+  s3.deleteObject(params, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(200).json({ status: 500, message: 'Failed to delete background: ' + id });
+    } else {
+      res.status(200).json({ status: 200, message: 'Background deleted successfully' });
+    }
+  });
+});
+
 router.post('/uploadStickers', upload.single('file'), function (req, res) {
     const file = req.file;
     const id = req.body.id;
@@ -72,6 +95,29 @@ router.post('/uploadStickers', upload.single('file'), function (req, res) {
   });
 });
 
+router.delete('/deleteStickers/:id', function (req, res) {
+  const id = req.params.id;
+
+  if (!id) {
+    res.status(200).json({ status: 400, message: 'Id must be required' });
+    return;
+  }
+
+  const params = {
+    Bucket: 'posters-assets',
+    Key: "stickers/" + id + ".png",
+  };
+
+  s3.deleteObject(params, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(200).json({ status: 500, message: 'Failed to delete stickers: ' + id });
+    } else {
+      res.status(200).json({ status: 200, message: 'Stickers deleted successfully' });
+    }
+  });
+});
+
 router.post('/uploadFonts', upload.single('file'), function (req, res) {
     const file = req.file;
     const id = req.body.id;
@@ -95,6 +141,29 @@ router.post('/uploadFonts', upload.single('file'), function (req, res) {
       res.status(200).json({ status: 500, message: 'Failed to upload Font: ' + file.originalname});
     } else {
       res.status(200).json({ status: 200, message: 'Font uploaded successfully', data: data.Location });
+    }
+  });
+});
+
+router.delete('/deleteFonts/:id', function (req, res) {
+  const id = req.params.id;
+
+  if (!id) {
+    res.status(200).json({ status: 400, message: 'Id must be required' });
+    return;
+  }
+
+  const params = {
+    Bucket: 'posters-assets',
+    Key: "fonts/" + id + ".ttf",
+  };
+
+  s3.deleteObject(params, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(200).json({ status: 500, message: 'Failed to delete fonts: ' + id });
+    } else {
+      res.status(200).json({ status: 200, message: 'Fonts deleted successfully' });
     }
   });
 });
