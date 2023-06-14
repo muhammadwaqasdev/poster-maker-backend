bab7bf2872621b5a6694d27100c0d44b28c98bee
macbook <muhammadwaqas0978@gmail.com>
2023-06-15 00:22:41 +0500

    catogory icon added

--- routes/upload.js
+++ routes/upload.js
@@ -168,4 +168,54 @@ router.delete('/deleteFonts/:id', function (req, res) {
   });
 });
 
+router.post('/uploadCatIcon', upload.single('file'), function (req, res) {
+    const file = req.file;
+    const id = req.body.id;
+
+  if (!(file && id)) {
+    res.status(200).json({ status: 400, message: 'File and Id must be required' });
+    return;
+  }
+
+  const params = {
+    Bucket: 'posters-assets',
+    Key: "category/" + id + ".png",
+    Body: file.buffer,
+    ContentType: 'font/png', 
+    ACL: 'public-read',
+  };
+
+  s3.upload(params, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(200).json({ status: 500, message: 'Failed to upload Category Icon: ' + file.originalname});
+    } else {
+      res.status(200).json({ status: 200, message: 'Category Icon uploaded successfully', data: data.Location });
+    }
+  });
+});
+
+router.delete('/deleteCatIcon/:id', function (req, res) {
+  const id = req.params.id;
+
+  if (!id) {
+    res.status(200).json({ status: 400, message: 'Id must be required' });
+    return;
+  }
+
+  const params = {
+    Bucket: 'posters-assets',
+    Key: "category/" + id + ".png",
+  };
+
+  s3.deleteObject(params, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(200).json({ status: 500, message: 'Failed to delete Category Icon: ' + id });
+    } else {
+      res.status(200).json({ status: 200, message: 'Category Icon deleted successfully' });
+    }
+  });
+});
+
 module.exports = router;
